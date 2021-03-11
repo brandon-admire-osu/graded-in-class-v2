3c01427dda68972b90596d02b74edba7562d68b0
Brandon Admire <admireb@oregonstate.edu>
2021-03-11 10:48:35 -0800

    updated test file

--- test_calc.py
+++ test_calc.py
@@ -1,12 +1,12 @@
-from calc import *
+import calc
 
 
 def test_add():
     for a, b in enumerate(range(10)):
-        assert add(a, b * 2) == a + b * 2
+        assert calc.add(a, b * 2) == a + b * 2
 
 
 def test_sub():
     for a, b in enumerate(range(10)):
-        assert sub(a, b * 2) == a - b * 2
+        assert calc.sub(a, b * 2) == a - b * 2
 
